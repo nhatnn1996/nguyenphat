0ec194dcd30e66153a2ed53c8cfd3004555884ab
truonghqGE <truonghq@galaxy.com.vn>
2022-04-27 08:56:29 +0700

    update

---
+++ pages/tin-tuc/index.js
@@ -0,0 +1,75 @@
+import React from 'react';
+const News = () => {
+  return (
+    <div>
+      <div class="large-9 col snipcss-oF6w9">
+        <div
+          id="row-1375434131"
+          class="row large-columns-3 medium-columns- small-columns-1 row-masonry"
+          data-packery-options='{"itemSelector": ".col", "gutter": 0, "presentageWidth" : true}'
+          style={{ position: 'relative', height: '2397.84px' }}
+        />
+        <div class="col post-item" style={{ position: 'absolute', left: '0px', top: '0px' }}>
+          <div class="col-inner">
+            <a href="https://nhaankhang.com/chinh-sach-giao-nhan-hang/" class="plain snip-a">
+              <div class="box box-text-bottom box-blog-post has-hover">
+                <div class="box-text text-left">
+                  <div class="box-text-inner blog-post-inner">
+                    <h5 class="post-title is-large snip-h5">CHÍNH SÁCH GIAO – NHẬN HÀNG</h5>
+                    <div class="post-meta is-small op-8">05/06/2021</div>
+                    <div class="is-divider"></div>
+                    <p class="from_the_blog_excerpt snip-p">
+                      Phạm vi áp dụng: Phạm vi áp dụng: tất cả mọi tỉnh thành trên cả [...]
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col post-item" style={{ position: 'absolute', left: '0px', top: '2060px' }}>
+          <div class="col-inner">
+            <a href="https://nhaankhang.com/quy-trinh-chong-tham-nha-ve-sinh/" class="plain snip-a">
+              <div class="box box-text-bottom box-blog-post has-hover">
+                <div class="box-image">
+                  <div class="image-cover" style={{ paddingTop: '56%' }}>
+                    <img
+                      width="300"
+                      height="215"
+                      src="https://nhaankhang.com/wp-content/uploads/2019/07/quy-trinh-chong-tham-nha-ve-sinh-1-1-300x215.jpg"
+                      data-src="https://nhaankhang.com/wp-content/uploads/2019/07/quy-trinh-chong-tham-nha-ve-sinh-1-1-300x215.jpg"
+                      class="attachment-medium size-medium wp-post-image lazy-load-active snip-img"
+                      alt=""
+                      loading="lazy"
+                      srcset="https://nhaankhang.com/wp-content/uploads/2019/07/quy-trinh-chong-tham-nha-ve-sinh-1-1-300x215.jpg 300w, https://nhaankhang.com/wp-content/uploads/2019/07/quy-trinh-chong-tham-nha-ve-sinh-1-1-600x429.jpg 600w, https://nhaankhang.com/wp-content/uploads/2019/07/quy-trinh-chong-tham-nha-ve-sinh-1-1.jpg 745w"
+                      data-srcset="https://nhaankhang.com/wp-content/uploads/2019/07/quy-trinh-chong-tham-nha-ve-sinh-1-1-300x215.jpg 300w, https://nhaankhang.com/wp-content/uploads/2019/07/quy-trinh-chong-tham-nha-ve-sinh-1-1-600x429.jpg 600w, https://nhaankhang.com/wp-content/uploads/2019/07/quy-trinh-chong-tham-nha-ve-sinh-1-1.jpg 745w"
+                      sizes="(max-width: 300px) 100vw, 300px"
+                    />
+                  </div>
+                </div>
+                <div class="box-text text-left">
+                  <div class="box-text-inner blog-post-inner">
+                    <h5 class="post-title is-large snip-h5">QUY TRÌNH CHỐNG THẤM NHÀ VỆ SINH</h5>
+                    <div class="is-divider"></div>
+                    <p class="from_the_blog_excerpt snip-p">
+                      Chống thấm nhà vệ sinh là một trong những công việc hết sức quan trọng, [...]
+                    </p>
+                  </div>
+                </div>
+                <div class="badge absolute top post-date badge-square">
+                  <div class="badge-inner">
+                    <span class="post-date-day">03</span>
+                    <br />
+                    <span class="post-date-month is-xsmall">Th7</span>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default News;
