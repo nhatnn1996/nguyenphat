c29ca417924281acbf89febb3b085595dddc4705
truonghqGE <truonghq@galaxy.com.vn>
2022-04-29 22:44:28 +0700

    update

---
+++ geters/product.js
@@ -0,0 +1,75 @@
+import { gql } from '@apollo/client';
+export const productGQL = gql`
+  query Product {
+    products {
+      nodes {
+        image {
+          altText
+          fileSize
+          sizes
+          slug
+          sourceUrl
+          srcSet
+          title
+          uri
+        }
+        shortDescription
+        link
+        menuOrder
+        id
+        sku
+        slug
+        status
+        name
+      }
+    }
+  }
+`;
+export const productDetailGQL = gql`
+  query Product($_id: ID!) {
+    product(id: $_id) {
+      image {
+        altText
+        fileSize
+        sizes
+        slug
+        sourceUrl
+        srcSet
+        title
+        uri
+      }
+      link
+      menuOrder
+      productCategories {
+        edges {
+          node {
+            id
+            slug
+            uri
+            name
+          }
+        }
+      }
+      description
+      shortDescription
+      galleryImages {
+        nodes {
+          mediaDetails {
+            file
+            meta {
+              aperture
+              camera
+              caption
+              title
+            }
+          }
+          date
+        }
+      }
+      sku
+      slug
+      status
+      name
+    }
+  }
+`;
