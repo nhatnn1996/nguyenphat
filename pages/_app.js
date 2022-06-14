8623b3d1e71d5659746f8fecba707fed4686bcfc
truonghqGE <truonghq@galaxy.com.vn>
2022-06-14 12:49:07 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css';
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import HeadSEO from '@/components/SEO/head';
 import Footer from '@/components/footer/index';
 import { useRouter } from 'next/router';
@@ -13,24 +13,40 @@ const apolloClient = new ApolloClient({
   uri: process.env.API_URL,
   cache: new InMemoryCache()
 });
-
+const isServer = () => typeof window === 'undefined';
 const variants = {
   hidden: { opacity: 0, x: 0, y: 0 },
   enter: { opacity: 1, x: 0, y: 0 },
   exit: { opacity: 0, x: 0, y: 0 }
 };
-
-MyApp.getInitialProps = async () => {
+const getPosts = async () => {
   const result = await apollo.query({ query: getInfoSetting });
   const infoSettings = result.data.user;
-  return { infoSettings };
+  return infoSettings
+}
+
+MyApp.getInitialProps = async (context) => {
+  if (isServer()) {
+    return {  infoSettings : await getPosts() };
+  } else {
+    return {
+      infoSettings: null
+    };
+  }
 };
 
 function MyApp({ Component, pageProps, infoSettings = {} }) {
-  const infoSetingsPage = infoSettings.setting_info;
+  const [renderPosts, setRenderPosts] = useState(infoSettings)
+  const infoSetingsPage = renderPosts.setting_info;
   if (typeof window !== 'undefined') {
     window.localStorage.setItem('info', JSON.stringify(infoSetingsPage));
   }
+  useEffect(() => {
+    if(infoSettings === null) {
+      getPosts()
+        .then(setRenderPosts)
+    }
+  }, [])
   const router = useRouter();
   const asPath = router.asPath;
   return (
