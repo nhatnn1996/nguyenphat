998f1840df71b798d044f63637855cb185a30433
nhatnn1996 <50484293+nhatnn1996@users.noreply.github.com>
2022-06-14 12:50:17 +0700

    Create local-build.sh

---
+++ local-build.sh
@@ -0,0 +1,20 @@
+VERSION=$2
+REGISTRY='ghcr.io'
+REGISTRY_PATH=‘image_path’
+IMAGE_NAME=image_name
+IMAGE=$REGISTRY/$REGISTRY_PATH/$IMAGE_NAME
+
+if [ -z "$2" ]; then
+  VERSION=$(cat package.json | grep version | head -1 | awk -F= "{ print $2 }" | sed 's/[version:,\",]//g' | tr -d '[[:space:]]')
+fi
+
+echo 'Building image '$IMAGE_NAME' version latest...'
+docker build -t $IMAGE:latest .
+docker push $IMAGE:latest
+
+echo 'Building image '$IMAGE_NAME' version '$VERSION'...'
+docker build -t $IMAGE:$VERSION .
+
+echo 'Deploying image '$IMAGE_NAME' version  '$VERSION'...'
+docker push $IMAGE:$VERSION
+echo 'Image ' ${IMAGE} ' version ' ${VERSION} ' build success!'
